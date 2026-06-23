bab0ce1b81580751a97aaea32b6a79e0279c4ba4
readywebsites <biz499websites@gmail.com>
2026-06-23 11:23:45 +0530

    categories add

--- main/models.py
+++ main/models.py
@@ -21,16 +21,16 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, blank=True)
 
-def save(self, *args, **kwargs):
-    if not self.slug:
-        self.slug = slugify(self.name)
-    super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-class Meta:
-    verbose_name_plural = "Categories"
+    class Meta:
+        verbose_name_plural = "Categories"
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
